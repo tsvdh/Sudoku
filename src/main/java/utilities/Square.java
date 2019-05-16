12716534b18bfe0e05e1e8af616acf1e561221ac
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-05-16 15:27:13 +0200

    add Square class

--- src/main/java/utilities/Square.java
+++ src/main/java/utilities/Square.java
@@ -1,8 +1,42 @@
 package utilities;
 
+import java.util.ArrayList;
+
 class Square {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private Integer value;
+
+    private ArrayList<Integer> options;
+
+    Square(Integer value) {
+        this.value = value;
+
+        if (value != null) {
+            this.options = null;
+        } else {
+            this.options = this.makeOptions();
+        }
+    }
+
+    private ArrayList<Integer> makeOptions() {
+        ArrayList<Integer> options = new ArrayList<>();
+
+        for (int i = 1; i < 10; i++) {
+            options.add(i);
+        }
+
+        return options;
+    }
+
+    Integer getValue() {
+        return this.value;
+    }
+
+    void removeOption(Integer option) {
+        this.options.remove(option);
+
+        if (this.options.size() == 1) {
+            this.value = this.options.get(0);
+        }
     }
 }
