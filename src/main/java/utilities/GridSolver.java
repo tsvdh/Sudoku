b45f591a000c675b63b68d11c7190a3af4e1093c
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-06-05 09:58:47 +0200

    minor tweaks

--- src/main/java/utilities/GridSolver.java
+++ src/main/java/utilities/GridSolver.java
@@ -20,11 +20,11 @@ public class GridSolver extends Observable {
     }
 
     public void solve() {
-        executor.scheduleWithFixedDelay(this ::updateNextSquare, 0, 250, TimeUnit.MILLISECONDS);
+        executor.scheduleWithFixedDelay(this :: updateNextSquare, 0, 200, TimeUnit.MILLISECONDS);
     }
 
     private void updateNextSquare() {
-        if (isSolved()) {
+        if (done()) {
             executor.shutdown();
 
             setChanged();
@@ -62,7 +62,7 @@ public class GridSolver extends Observable {
         }
     }
 
-    private boolean isSolved() {
+    private boolean done() {
         for (Square square : grid.getSquareList()) {
             if (square.hasNoValue()) {
                 return false;
