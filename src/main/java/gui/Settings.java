1ed48cf90c0138ad7d1cec43afc8948439002cc2
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-07-17 18:32:25 +0100

    add spacing and description for settings screen

--- src/main/java/gui/Settings.java
+++ src/main/java/gui/Settings.java
@@ -6,9 +6,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import utilities.SettingsHandler;
@@ -58,16 +60,45 @@ class Settings extends Observable {
 
         CheckBox checkBox = new CheckBox();
         checkBox.setText("Slow mode");
-        checkBox.setPrefSize(80, 40);
+        checkBox.setPrefHeight(40);
+        checkBox.setFont(new Font(18));
         if (handler.getMode().equals("slow")) {
             checkBox.setSelected(true);
         }
 
+        Text description1 = new Text();
+        description1.setText("The amount of milliseconds between each step");
+        description1.setFont(new Font(15));
+
+        Label description2 = new Label();
+        description2.setText("The amount of seconds to pause after solving a red square");
+        description2.setFont(new Font(15));
+        description2.setPadding(new Insets(50, 0, 0, 0));
+
+        Button line1 = new Button();
+        line1.setPrefSize(300, 1);
+        line1.setFont(new Font(0));
+
+        Button line2 = new Button();
+        line2.setPrefSize(300, 1);
+        line2.setFont(new Font(0));
+
+        Button filler = new Button();
+        filler.setVisible(false);
+        filler.setPrefSize(0, 0);
+        filler.setFont(new Font(0));
+        filler.setPadding(new Insets(50, 0, 0, 0));
+
         GridPane gridPane = new GridPane();
-        gridPane.add(intervalSlider, 0, 0);
-        gridPane.add(pauseSlider, 0, 1);
-        gridPane.add(checkBox, 0, 2);
-        gridPane.add(okButton, 1, 3);
+        gridPane.add(description1, 0, 0);
+        gridPane.add(intervalSlider, 0, 1);
+        gridPane.add(line1, 0, 2);
+        gridPane.add(description2, 0, 3);
+        gridPane.add(pauseSlider, 0, 4);
+        gridPane.add(line2, 0, 5);
+        gridPane.add(filler, 0, 6);
+        gridPane.add(checkBox, 0, 7);
+        gridPane.add(okButton, 1, 8);
         gridPane.setPadding(new Insets(20));
         gridPane.setAlignment(Pos.CENTER);
 
