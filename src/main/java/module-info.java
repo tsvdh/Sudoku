7b57f4104be43f505ef97d99f966e1fe3087447a
Tobias van den Hurk <tsvdhurk@gmail.com>
2020-12-28 11:51:33 +0100

    maven package creates image with moditect using modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sudoku {
+    requires org.jetbrains.annotations;
+    requires dropbox.core.sdk;
+    requires javafx.controls;
+    requires javatuples;
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+    requires mslinks;
+
+    exports update;
+}
